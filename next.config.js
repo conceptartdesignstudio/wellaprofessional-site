7f4afbff47b8ae75e59e7c6d5dedf49a81b6faed
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2022-08-24 00:38:51 -0300

    chore: initial error handling

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 module.exports = {
   reactStrictMode: false,
   output: 'standalone',
-  future: { webpack5: true },
   images: {
     domains: ["localhost", process.env.NEXT_PUBLIC_WELLA_ENV],
   },
-  plugins: [
-    [
-      "babel-plugin-styled-components",
-      {
-        ssr: false,
-      },
-    ],
-  ],
 };
---
+++ pages/_error.js
@@ -0,0 +1,18 @@
+const Error = ({statusCode}) => {
+  return (
+    <>
+      { statusCode === 404 &&
+        (
+          <h1>Esse produto não existe ou você está visitando a página errada.</h1>
+        )
+      }
+    </>
+  )
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode }
+}
+
+export default Error;
