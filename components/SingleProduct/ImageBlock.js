a48aec345f4949b81559f4728113dce5801b77ee
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2022-09-02 14:15:00 -0300

    chore: remove margin from image block

--- components/SingleProduct/ImageBlock.js
+++ components/SingleProduct/ImageBlock.js
@@ -4,7 +4,7 @@ export const ImageAsBlock = ({ images }) => {
   return (
     <div className="flex flex-col mt-6 justify-center">
       {images.map(({ id, altText, mediaItemUrl }) => (
-        <div key={id} className="relative max-w-[400px] my-3">
+        <div key={id} className="relative max-w-[400px]">
           <Image
             src={mediaItemUrl}
             alt={altText}
