24af0bb01b4f40bde9069b0bc69a09badd19f2e5
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2022-12-16 22:32:51 -0300

    chore: final changes to pp icon

--- components/Icons/Products/PP.js
+++ components/Icons/Products/PP.js
@@ -3,9 +3,11 @@ export const PpSVG = ({ selectedColor }) => {
     <>
       <svg
         version="1.1"
-        id="others"
+        id="pet"
+        x="0px"
+        y="0px"
         viewBox="0 0 300 300"
-        enableBackground="new 0 0 300 300"
+        enableBackground="new 0 0 300 300;"
         xmlSpace="preserve"
         width="35px"
         height="35px"
@@ -13,49 +15,71 @@ export const PpSVG = ({ selectedColor }) => {
         <g>
           <g>
             <defs>
-              <rect id="SVGID_1_" x="-0.32" y="0" width="300" height="300" />
+              <rect id="SVGID_1_" x="0.12" y="-0.47" width="300" height="300" />
             </defs>
-            <clipPath id="SVGID_00000011021960408860289350000010086379232425434035_">
-              <use className="visible" xlinkHref="#SVGID_1_" />
+            <clipPath id="SVGID_00000051349318086511092560000010309821066309863073_">
+              <use xlinkHref="#SVGID_1_" className="visible" />
             </clipPath>
 
             <path
               className="st0"
               stroke={selectedColor}
               d="
-			M167.27,206.9h60.11c17.74,0,28.93-19.07,20.3-34.56l-40.21-72.13 M80.08,128.53l-24.42,43.81c-8.64,15.49,2.56,34.56,20.3,34.56
-			h41.61 M185.81,61.36l-13.84-24.83c-8.86-15.9-31.73-15.9-40.6,0l-27.58,49.48"
+			M167.71,206.43h60.11c17.74,0,28.93-19.07,20.3-34.56L207.9,99.74 M80.52,128.06l-24.42,43.81c-8.64,15.49,2.56,34.56,20.3,34.56
+			H118 M186.24,60.89L172.4,36.06c-8.86-15.9-31.73-15.9-40.6,0l-27.58,49.48"
             />
           </g>
           <polygon
             fill={selectedColor}
             className="st1"
-            points="167.12,206.9 166.97,183.23 138.94,207.08 167.27,230.56 167.12,206.9 	"
+            points="167.56,206.43 167.41,182.76 139.38,206.61 167.71,230.09 167.56,206.43 	"
           />
           <polygon
             fill={selectedColor}
             className="st1"
-            points="78.29,131.62 100.11,142.88 91.69,105.62 56.46,120.36 78.29,131.62 	"
+            points="78.72,131.15 100.55,142.41 92.13,105.16 56.9,119.89 78.72,131.15 	"
           />
           <polygon
             fill={selectedColor}
             className="st1"
-            points="181.8,56.56 160.16,69.09 196.73,82.34 203.45,44.02 181.8,56.56 	"
+            points="182.24,56.09 160.59,68.63 197.17,81.87 203.89,43.55 182.24,56.09 	"
           />
           <text
             fill={selectedColor}
-            transform="matrix(1 0 0 1 61.6624 281.6485)"
+            transform="matrix(1 0 0 1 110.3895 281.1809)"
             className="st1 st2 st3 st4"
           >
-            POTE
+            PO
           </text>
           <text
             fill={selectedColor}
-            transform="matrix(1 0 0 1 128.1858 165.25)"
+            transform="matrix(1 0 0 1 165.9989 281.1809)"
+            className="st1 st2 st3"
+          >
+            TE
+          </text>
+          <text
+            fill={selectedColor}
+            transform="matrix(1 0 0 1 127.241 164.5324)"
             className="st1 st2 st5"
           >
             5
           </text>
+          <polygon
+            fill={selectedColor}
+            className="st1"
+            points="167.56,206.43 167.41,182.76 139.38,206.61 167.71,230.09 167.56,206.43 	"
+          />
+          <polygon
+            fill={selectedColor}
+            className="st1"
+            points="78.72,131.15 100.55,142.41 92.13,105.16 56.9,119.89 78.72,131.15 	"
+          />
+          <polygon
+            fill={selectedColor}
+            className="st1"
+            points="182.24,56.09 160.59,68.63 197.17,81.87 203.89,43.55 182.24,56.09 	"
+          />
         </g>
       </svg>
     </>
